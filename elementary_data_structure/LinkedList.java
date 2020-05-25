b9a364cd9503e5575eeac46387960d3870abed45
ksak54 <kumarsaksham54@gmail.com>
2020-05-25 21:31:57 +0530

    Deletion in LinkedList

--- elementary_data_structure/LinkedList.java
+++ elementary_data_structure/LinkedList.java
@@ -29,9 +29,38 @@ public class LinkedList {
 
     public static void printLinkedList(LinkedList list) {
         Node curr_node = list.head;
-        while (curr_node.next != null) {
-            System.out.println(curr_node.data + " ");
-            curr_node = curr_node.next;
+        System.out.println("\nLinked List: ");
+        if (curr_node != null) {
+            do {
+                System.out.print(curr_node.data + " ");
+                curr_node = curr_node.next;
+            } while (curr_node != null);
+        } else {
+            System.out.println("Linked List is empty.");
+        }
+        System.out.println();
+
+    }
+
+    public static void delete(LinkedList list, int key) {
+        Node del_node = list.head.next;
+        Node prev = list.head;
+        if (list.head.data == key) {
+            list.head = list.head.next;
+        }
+
+        else {
+            while (del_node != null) {
+                if (del_node.data == key) {
+                    prev.next = del_node.next;
+                    break;
+                } else if (del_node.next != null) {
+                    prev = prev.next;
+                } else {
+                    System.out.println(key + " not found");
+                }
+                del_node = del_node.next;
+            }
         }
 
     }
@@ -39,22 +68,29 @@ public class LinkedList {
     public static void main(String[] args) {
 
         LinkedList list = new LinkedList();
-
-        //
-        // ******INSERTION******
-        //
-
-        // Insert the values
-        list = insert(list, 1);
-        list = insert(list, 2);
-        list = insert(list, 3);
-        list = insert(list, 4);
+        // list = insert(list, 1);
+        // list = insert(list, 2);
+        // list = insert(list, 3);
+        // list = insert(list, 4);
         list = insert(list, 5);
         list = insert(list, 6);
         list = insert(list, 7);
         list = insert(list, 8);
 
-        // Print the LinkedList
         printLinkedList(list);
+
+        delete(list, 1);
+        delete(list, 2);
+        delete(list, 3);
+        delete(list, 4);
+        delete(list, 5);
+        // delete(list, 6);
+        // delete(list, 7);
+        delete(list, 8);
+        delete(list, 6);
+        delete(list, 7);
+
+        printLinkedList(list);
+
     }
 }
