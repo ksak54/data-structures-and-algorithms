4400d1974c8d5de380e5dbfe66b680a1302fa4f2
ksak54 <kumarsaksham54@gmail.com>
2020-05-25 20:25:56 +0530

    Insertion in LinkedList

---
+++ elementary_data_structure/LinkedList.java
@@ -0,0 +1,60 @@
+public class LinkedList {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static LinkedList insert(LinkedList list, int data) {
+        Node new_node = new Node(data);
+        if (list.head == null) {
+            list.head = new_node;
+        }
+
+        else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = new_node;
+        }
+        return list;
+    }
+
+    public static void printLinkedList(LinkedList list) {
+        Node curr_node = list.head;
+        while (curr_node.next != null) {
+            System.out.println(curr_node.data + " ");
+            curr_node = curr_node.next;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+
+        //
+        // ******INSERTION******
+        //
+
+        // Insert the values
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+        list = insert(list, 6);
+        list = insert(list, 7);
+        list = insert(list, 8);
+
+        // Print the LinkedList
+        printLinkedList(list);
+    }
+}
